4a9c73fdf0a76d8fea8594e00622a1e07c84039b
eelaukka <eelaukka@student.oulu.fi>
2017-10-10 14:24:22 +0300

    the right port god damnit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!')
 })
 app.use(express.static('public'))
